e6e0425cbc06fb96170be1b04c87dc0ee71a40c4
Tom-Petty98 <tompetty527@yahoo.co.uk>
2026-01-19 12:30:58 +0000

    Added an alt solution which is more single responsibility

--- main.go
+++ main.go
@@ -1,46 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
 	"time"
 
-	pokecache "github.com/Tom-Petty98/pokedexcli/internal"
+	"github.com/Tom-Petty98/pokedexcli/internal/pokeapi"
 )
 
-var cache *pokecache.Cache
-
 func main() {
-	userInput := bufio.NewScanner(os.Stdin)
-	cache = pokecache.NewCache(1 * time.Minute)
-
-	for {
-		fmt.Print("Pokedex > ")
-		if !userInput.Scan() {
-			break
-		}
-
-		text := userInput.Text()
-		words := cleanInput(text)
-		if len(words) > 0 {
-			value, exists := commands[words[0]]
-			if exists {
-				args := words[1:]
-
-				if err := value.callback(args); err != nil {
-					fmt.Printf("%s\n", err)
-				}
-
-			} else {
-				fmt.Print("Unknown command\n")
-			}
-		}
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
+	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
+		pokeapiClient: pokeClient,
 	}
-}
 
-func cleanInput(text string) []string {
-	text = strings.ToLower(strings.TrimSpace(text))
-	return strings.Fields(text)
+	startRepl(cfg)
 }
